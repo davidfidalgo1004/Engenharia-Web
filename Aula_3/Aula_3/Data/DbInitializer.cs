08c8ef582fa78cb5d174acc97787eacc00632b70
David Fidalgo <160844915+davidfidalgo1004@users.noreply.github.com>
2024-10-06 17:27:18 +0100

    Aula 3

---
+++ Aula_3/Aula_3/Data/DbInitializer.cs
@@ -0,0 +1,38 @@
+﻿using Aula_3.Models;
+
+namespace Aula_3.Data
+{
+    public class DbInitializer
+    {
+        private readonly Aula_3Context _context; //CLASSE->NOME PROJETO
+
+        public DbInitializer(Aula_3Context context)
+        {
+            _context = context;
+        }   
+
+        public void Run()
+        {
+            _context.Database.EnsureCreated();
+            if (_context.Category.Any()) //Vai verificar se existe dados na base dados, caso exista, não adiciona as informações abaxixo do if
+            {
+                return;
+            }
+            var category = new Category[]
+            {
+                new Category{Name="Programming", Description="Algoritms and programming area courses"},
+                new Category{Name="Administration", Description="Public administration and business management course"},
+                new Category{Name="Communication", Description="Business  and institutional communication course"}
+
+            };
+            _context.Category.AddRange(category);
+            _context.SaveChanges();
+            //foreach(var c in category)  Aqui caso queiramos introduzir um a um
+        }
+
+
+
+    }
+
+
+}
---
+++ Aula_3/Aula_3/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aula_3.Models
+{
+    public class Category
+    {
+        [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.None)] Nesta linha se quisermos que seja introduzido pelo utilizador
+        public int Id { get; set; } //É redondante, um campo chamado Id é sempre a chave primária
+        [Required(ErrorMessage ="Required field")]
+        [StringLength(50, MinimumLength =3, ErrorMessage ="{0} length must be between {2} and {1}")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(256, ErrorMessage ="{0} lenght can not exceed {1} characteres")]
+        public string? Description { get; set; }
+        public Boolean State { get; set; } = true;
+
+        [DisplayName("Creation Name")]
+        public DateTime Date {  get; set; }=DateTime.Now;
+        
+    }
+}
